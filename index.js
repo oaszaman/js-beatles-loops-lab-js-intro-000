8c4d60416c2eae77829cc845a9c8d602d459909f
Shahzab Zaman <o.aszaman00@gmail.com>
2021-04-06 20:49:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // add solution here
-
+let musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+let instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments) {
   let array = [];
   for (let i = 0; i < musicians.length; i++){
