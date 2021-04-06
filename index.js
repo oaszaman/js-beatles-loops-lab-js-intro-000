020638c491346cb5666d8d22b768650a728e6b80
Shahzab Zaman <o.aszaman00@gmail.com>
2021-04-06 20:44:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // add solution here
-function theBeatlesPlay() {
+
+function theBeatlesPlay(musicians, instruments) {
+  let array = [];
+  for (let i = 0; i < musicians.length; i++){
+    array.push(musicians[i] + " plays " + instruments[i]):
+  }
 
 }
 
